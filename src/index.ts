583e3dfc3449e47b189c6426b027d640b0a7b52b
Peter Sieg <chasingmaxwell@gmail.com>
2020-01-31 17:38:28 -0500

    fix: refactor canvas drawing to accurately show the perceptron understanding

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,63 @@ import http from "http";
 import { Perceptron } from "./brain";
 import { createCanvas } from "canvas";
 
-const perceptron = new Perceptron();
+const perceptron = new Perceptron({
+  lossFn: (i1: number, i2: number): number => (i1 > i2 ? i1 - i2 : i2 - i1)
+});
 
 const canvas = createCanvas(800, 800);
 const ctx = canvas.getContext("2d");
 
-ctx.strokeStyle = "rgba(0,0,0,0.5)";
-ctx.beginPath();
-ctx.lineTo(800, 800);
-ctx.lineTo(0, 0);
-ctx.stroke();
+type dot = { x: number; y: number; color: string };
+const dots: dot[] = [];
+function addDot(x: number, y: number, color: string): void {
+  dots.push({ x, y, color });
+}
 
-function addDot() {
+function redraw(): void {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // visualize correct answer
+  ctx.strokeStyle = "rgba(0,0,0,0.5)";
+  ctx.beginPath();
+  ctx.moveTo(800, 800);
+  ctx.lineTo(0, 0);
+  ctx.stroke();
+
+  // visualize understanding
+  ctx.strokeStyle = "rgba(100,0,100,0.5)";
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  // @TODO: this doesn't take the bias into account but still happens to seem
+  // correct. Figure that out!
+  ctx.lineTo(800, -(800 * perceptron.w1) / perceptron.w2);
+  ctx.stroke();
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#003300";
+  for (const dot of dots) {
+    ctx.fillStyle = dot.color;
+    ctx.beginPath();
+    ctx.arc(dot.x, dot.y, 5, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.stroke();
+  }
+}
+
+function guess(): void {
   const x = Math.random() * 800;
   const y = Math.random() * 800;
   const [guess] = perceptron.train(x, y, x > y ? 1 : -1);
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = "#003300";
-  ctx.fillStyle = guess === 1 ? "green" : "red";
-  ctx.beginPath();
-  ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
-  ctx.fill();
-  ctx.stroke();
+  addDot(x, y, guess === 1 ? "green" : "red");
 }
 
 http
   .createServer(function(req, res) {
     if (req.url === "/") {
       res.writeHead(200, { "Content-Type": "text/html" });
-      addDot();
+      guess();
+      redraw();
+
       res.end(
         `<p><b>weight 1: </b>${perceptron.w1}</p>` +
           `<p><b>weight 2: </b>${perceptron.w2}</p>` +
